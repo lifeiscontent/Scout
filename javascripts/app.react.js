cd7c5f49bf9607be6bb0cfd65408a700df883943
Aaron Reisman <aaron@lifeiscontent.net>
2017-10-20 06:08:13 +0700

    Update plugin

--- javascripts/app.react.js
+++ javascripts/app.react.js
@@ -1,12 +1,12 @@
 const { Component } = React;
 
-const findPosition = (node) => {
+const findPosition = node => {
   let offsetLeft = node.offsetLeft;
   let offsetTop = node.offsetTop;
 
-  while( node.offsetParent ) {
-    if( node == document.body ) {
-      break
+  while (node.offsetParent) {
+    if (node == document.body) {
+      break;
     } else {
       offsetLeft = offsetLeft + node.offsetParent.offsetLeft;
       offsetTop = offsetTop + node.offsetParent.offsetTop;
@@ -16,9 +16,9 @@ const findPosition = (node) => {
 
   return {
     offsetLeft,
-    offsetTop,
+    offsetTop
   };
-}
+};
 
 class ScoutInfoPanel extends Component {
   constructor(props) {
@@ -35,33 +35,68 @@ class ScoutInfoPanel extends Component {
         position: 'fixed',
         right: 0,
         top: 0,
-        zIndex: 99999999,
+        zIndex: 99999999
       },
       link: {
         color: '#3b99fc',
-        float: 'right',
+        float: 'right'
+      },
+      key: {
+        fontWeight: 700
+      },
+      value: {
+        color: '#CCCCCC'
       }
     };
     this.state = {
-      helpIsActive: false,
+      helpIsActive: false
     };
     this.handleHelpClick = this.handleHelpClick.bind(this);
   }
   handleHelpClick(event) {
-    this.setState({helpIsActive: !this.state.helpIsActive});
+    this.setState({ helpIsActive: !this.state.helpIsActive });
   }
   render() {
-    return <div style={ this.style.root }>
-      <details onClick={ this.handleHelpClick } style={ {display: `${this.state.helpIsActive ? 'block' : 'inline-block'}`} }>
-        <summary style={ {color: '#CCCCCC', paddingRight: 6, fontWeight: 700} }>Scout</summary>
-        <div style={ {marginTop: 6, marginBottom: 6} }><strong>Change hue</strong>: <kbd>Shift+C</kbd> <strong>Change opacity</strong>: <kbd>Shift+X</kbd> <strong>Close</strong>: <kbd>Esc</kbd> </div>
-      </details>
-      {Object.keys(this.props.data).map((key) => {
-        return <span><strong>{key}</strong>: {this.props.data[key]} </span>;
-      })}
-      <a style={this.style.link} href="https://hired.com/x/On0PYI" target="_blank">Looking for a job?</a>
-      <div style={ {clear: 'both'} } />
-    </div>;
+    return (
+      <div style={this.style.root}>
+        <details
+          onClick={this.handleHelpClick}
+          style={{
+            display: `${this.state.helpIsActive ? 'block' : 'inline-block'}`
+          }}
+        >
+          <summary
+            style={{ color: '#CCCCCC', paddingRight: 6, fontWeight: 700 }}
+          >
+            Scout
+          </summary>
+          <div style={{ marginTop: 6, marginBottom: 6 }}>
+            <strong style={this.style.key}>Change hue</strong>:{' '}
+            <kbd style={this.style.value}>Shift+C</kbd>{' '}
+            <strong style={this.style.key}>Change opacity</strong>:{' '}
+            <kbd style={this.style.value}>Shift+X</kbd>{' '}
+            <strong style={this.style.key}>Close</strong>:{' '}
+            <kbd style={this.style.value}>Esc</kbd>{' '}
+          </div>
+        </details>
+        {Object.entries(this.props.data).map(([key, value]) => {
+          return (
+            <span>
+              <strong style={this.style.key}>{key}</strong>:{' '}
+              <span style={this.style.value}>{value} </span>
+            </span>
+          );
+        })}
+        <a
+          style={this.style.link}
+          href="https://hired.com/x/On0PYI"
+          target="_blank"
+        >
+          Looking for a job?
+        </a>
+        <div style={{ clear: 'both' }} />
+      </div>
+    );
   }
 }
 
@@ -73,8 +108,8 @@ class Scout extends Component {
       opacity: 0.3,
       style: {
         pointerEvents: 'none',
-        position: 'absolute',
-      },
+        position: 'absolute'
+      }
     };
     this.handleDocumentMouseMove = this.handleDocumentMouseMove.bind(this);
     this.handleDocumentKeyDown = this.handleDocumentKeyDown.bind(this);
@@ -82,44 +117,32 @@ class Scout extends Component {
   handleDocumentKeyDown(event) {
     let key = event.which || event.keyCode || 0;
     switch (true) {
-      case (event.shiftKey && key === 67):
-        this.setState({hue: (this.state.hue + 10) % 360});
-      break;
-      case (event.shiftKey && key === 67):
-        this.setState({opacity: (((this.state.opacity * 100) + 10) % 110) / 100});
-      break;
+      case event.shiftKey && key === 88:
+        this.setState({ hue: (this.state.hue + 10) % 360 });
+        break;
+      case event.shiftKey && key === 67:
+        this.setState({
+          opacity: ((this.state.opacity * 100 + 10) % 110) / 100
+        });
+        break;
     }
   }
   handleDocumentMouseMove(event) {
-    const {
-      offsetHeight: height,
-      offsetWidth: width,
-    } = event.target;
-    const {
-      offsetLeft: left,
-      offsetTop: top,
-    } = findPosition(event.target);
+    const { offsetHeight: height, offsetWidth: width } = event.target;
+    const { offsetLeft: left, offsetTop: top } = findPosition(event.target);
     this.setState({
       style: {
         ...this.state.style,
         height,
         left,
         top,
-        width,
+        width
       }
     });
   }
   componentDidMount() {
-    document.addEventListener(
-      'mousemove',
-      this.handleDocumentMouseMove,
-      false
-    );
-    document.addEventListener(
-      'keydown',
-      this.handleDocumentKeyDown,
-      false
-    );
+    document.addEventListener('mousemove', this.handleDocumentMouseMove, false);
+    document.addEventListener('keydown', this.handleDocumentKeyDown, false);
   }
   componentWillUnmount() {
     document.removeEventListener(
@@ -127,34 +150,29 @@ class Scout extends Component {
       this.handleDocumentMouseMove,
       false
     );
-    document.removeEventListener(
-      'keydown',
-      this.handleDocumentKeyDown,
-      false
-    );
+    document.removeEventListener('keydown', this.handleDocumentKeyDown, false);
   }
   render() {
-    const {
-      style,
-      opacity,
-      hue,
-    } = this.state;
+    const { style, opacity, hue } = this.state;
+    const { width, height, left, top } = style;
     const data = {
-      width: style.width,
-      height: style.height,
-      left: style.left,
-      top: style.top,
+      width,
+      height,
+      left,
+      top,
       opacity,
-      hue,
+      hue
     };
 
     return (
       <div>
-        <div style={ {
-          ...style,
-          backgroundColor: `hsla(${this.state.hue}, 100%, 50%, ${this.state.opacity})`
-        } }></div>
-        <ScoutInfoPanel data={ data } />
+        <div
+          style={{
+            ...style,
+            backgroundColor: `hsla(${hue}, 100%, 50%, ${opacity})`
+          }}
+        />
+        <ScoutInfoPanel data={data} />
       </div>
     );
   }
